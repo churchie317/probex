25af57ac960c020c50abbb74dac19bd69bec39f1
Eric Churchill <functor@Erics-MacBook-Pro.local>
2019-05-06 00:06:18 -0700

    ensure registry started before children start

--- lib/probex/supervisor.ex
+++ lib/probex/supervisor.ex
@@ -4,26 +4,29 @@ defmodule Probex.Supervisor do
   use Supervisor
 
   def start_link(opts) do
-    Supervisor.start_link(__MODULE__, opts)
+    config = config_opts(opts)
+
+    with {:ok, true} <- ensure_started(),
+         {:ok, true} <- ensure_unused(config.name),
+         do: Supervisor.start_link(__MODULE__, config)
   end
 
   @impl true
-  def init(opts) do
-    config = config_opts(opts)
-
-    children = [
-      {Probex.ProbeServer, config.name},
-      {Plug.Cowboy,
-       scheme: config.scheme,
-       plug: {Probex.Plug, config.name},
-       options: [
-         port: config.port,
-         timeout: config.timeout,
-         transport_options: [num_acceptors: 5]
-       ]}
-    ]
-
-    Supervisor.init(children, strategy: :rest_for_one)
+  def init(config) do
+    Supervisor.init(
+      [
+        {Probex.ProbeServer, config.name},
+        {Plug.Cowboy,
+         scheme: config.scheme,
+         plug: {Probex.Plug, config.name},
+         options: [
+           port: config.port,
+           timeout: config.timeout,
+           transport_options: [num_acceptors: 5]
+         ]}
+      ],
+      strategy: :rest_for_one
+    )
   end
 
   defp config_opts(opts) do
@@ -32,5 +35,20 @@ defmodule Probex.Supervisor do
     |> Keyword.put_new(:scheme, :http)
     |> Keyword.put_new(:timeout, 5_000)
     |> Enum.into(%{})
+  end
+
+  defp ensure_started() do
+    if Probex.Registry.started?() do
+      {:ok, true}
+    else
+      {:error, false}
+    end
+  end
+
+  defp ensure_unused(name) do
+    case Registry.lookup(Registry.Probex, name) do
+      [] -> {:ok, true}
+      [{pid, _}] -> {:error, {:already_started, pid}}
+    end
   end
 end
